5322b40b5e1f186d87bc27e4b7d131e85f71b4a8
diabhalothia1 <100493743+diabhalothia1@users.noreply.github.com>
2024-10-29 11:32:19 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
+        <h1>Welcome to My React App!</h1>
+        <p>This is the homepage of your React app.</p>
+        <button onClick={() => alert("Hello, world!")}>Click Me</button>
       </header>
     </div>
   );
